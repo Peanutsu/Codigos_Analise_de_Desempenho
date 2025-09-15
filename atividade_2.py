403931839915f8ec89892b9457b6f9e8fc9f304b
Adriano Junior <118844854+Peanutsu@users.noreply.github.com>
2025-09-15 04:43:06 +0000

    codigo leitor de arquivo para modificação de lista encadeada

---
+++ atividade_2.py
@@ -0,0 +1,70 @@
+class No:
+    def __init__(self, valor):
+        self.valor = valor
+        self.proximo = None
+
+class ListaEncadeada:
+    def __init__(self):
+        self.cabeca = None
+    
+    def adicionar(self, valor, posicao):
+        novo_no = No(valor)
+        
+        if posicao == 0:
+            novo_no.proximo = self.cabeca
+            self.cabeca = novo_no
+            return
+        
+        atual = self.cabeca
+        for i in range(posicao - 1):
+            if atual is None:
+                return
+            atual = atual.proximo
+        
+        if atual is None:
+            return
+        
+        novo_no.proximo = atual.proximo
+        atual.proximo = novo_no
+    
+    def remover(self):
+        if self.cabeca is None:
+            return
+        self.cabeca = self.cabeca.proximo
+    
+    def imprimir(self):
+        valores = []
+        atual = self.cabeca
+        while atual:
+            valores.append(atual.valor)
+            atual = atual.proximo
+        print(valores)
+
+def processar_arquivo(nome_arquivo):
+    lista = ListaEncadeada()
+    
+    with open(nome_arquivo, 'r') as arquivo:
+        linhas = arquivo.readlines()
+    
+
+    valores_iniciais = list(map(int, linhas[0].strip().split()))
+    for valor in valores_iniciais:
+        lista.adicionar(valor, 0)
+    
+
+    quantidade_acoes = int(linhas[1].strip())
+    
+
+    for i in range(2, 2 + quantidade_acoes):
+        linha = linhas[i].strip().split()
+        acao = linha[0]
+        
+        if acao == 'A':
+            valor = int(linha[1])
+            posicao = int(linha[2])
+            lista.adicionar(valor, posicao)
+        elif acao == 'R':
+            lista.remover()
+        elif acao == 'P':
+            lista.imprimir()
+processar_arquivo("blabla.txt")
